95f6179e75f521138eddd44d895cca1d307e1a8f
Soulscr1bbl3s <Almhaisen88@gmail.com>
2024-08-17 22:29:28 -0400

    Update Scripts.js

--- Scripts.js
+++ Scripts.js
@@ -1,2 +1,9 @@
-
+window.onload = function() {
+    // Text fades in and out automatically with CSS animation.
+    
+    // Wait for the text animation to finish (4 seconds)
+    setTimeout(function() {
+        // Start ballerina animation (already set in CSS)
+    }, 4000);
+};
 
